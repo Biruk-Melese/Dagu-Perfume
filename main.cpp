47e7e5f7d39c6abcc0d117de81b3f5483dfdbb18
Biruk-Melese <biruk.melese.m@gmail.com>
2025-10-08 07:36:33 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+cout << "Enter Two Numbers x and y :"<<endl;
+int x, y;
+cin>>x, y;
+
+double z = ((x>y)? 1 : 444);
+cout<<z;
+return 1;
+
+}
+
